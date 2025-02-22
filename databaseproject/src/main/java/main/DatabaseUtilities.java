a5ae89d13a6c4f0dd78a6f177ec2eeecfddf5787
NNLisko <niko.lausto@student.lut.fi>
2025-02-22 19:31:55 +0200

    add songs works now

--- databaseproject/src/main/java/main/DatabaseUtilities.java
+++ databaseproject/src/main/java/main/DatabaseUtilities.java
@@ -6,16 +6,61 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.SQLException;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DatabaseUtilities {
 
     public static void addSong(String name, String artistName, String genre, String album, String producer,
             String writer, String publisher, Integer songlength, Connection conn) {
-            String query = "INSERT INTO song (songID, songName, artistID, albumName, genreName, producerName, writerName, publisherName, dateOfPublish, songlength) VALUES (";
-            
-            query += "'" + name + "', '" + artistName + "', '" + album + "', '" + genre + "', '" + producer + "', '" + writer + "', '" + publisher + "', '" + "today" + "', " + songlength + ");" ;
-            System.out.println(query);
-        }
+            String query = "INSERT INTO song (songID, songName, artistID, albumName, genreName, producerName, writerName, publisherName, dateOfPublish, songlength) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            boolean exists = true;
+            String id = null;
+            /* make a unique and nonexisting id */
 
+            /* checks whether id exists with prepared statement */
+
+            LocalDate today = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    
+            while (exists) {
+                id = Integer.toString((int) (Math.random() * 10000));
+                String checkQuery = "SELECT COUNT(*) FROM song WHERE songID = ?";
+                
+                try (PreparedStatement ps = conn.prepareStatement(checkQuery)) {
+                    ps.setString(1, id);
+                    ResultSet rs = ps.executeQuery();
+                    rs.next();
+                    if (rs.getInt(1) > 0) {
+                        exists = true;
+                    }else {exists = false;}  
+                } catch (SQLException e) {
+                    System.err.println(e);
+                    System.err.println("failed to fetch songs");
+                }
+            }
+
+            /* sets the parameters and then executes th e prepared statement */
+
+            try {
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, id);
+                ps.setString(2, name);
+                ps.setString(3, artistName);
+                ps.setString(4, genre);
+                ps.setString(5, album);
+                ps.setString(6, producer);
+                ps.setString(7, writer);
+                ps.setString(8, publisher);
+                ps.setString(9, today.format(formatter));
+                ps.setInt(10, songlength);
+                ps.executeUpdate();
+                System.out.println("Song added");
+            } catch (Exception e) {
+                System.out.println("Error adding the song");
+            }
+            App.cp.returnConnection(conn);
+        }
 
     public static void addArtist(String name, String email) {
 
